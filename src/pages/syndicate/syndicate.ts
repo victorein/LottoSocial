bb7da7076f4776f80331a82908cdd4af7535ae1a
alex-mtchell <alexander.mitchell.dev@outlook.com>
2018-06-13 03:29:25 +0700

    add assets files

--- src/pages/syndicate/syndicate.ts
+++ src/pages/syndicate/syndicate.ts
@@ -9,33 +9,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SyndicatePage {
   cards: any = [];
   itemExpandHeight: number = 82;
+  dueImage = 'assets/img/status_due.png';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.cards = [
-      {
-        id: 0,
-        image: "assets/imgs/syndicate/generic_syndicate.jpg",
-        label: "ZULA0280",
-        created: "May 29, 2018",
-        endDate: "oneoff",
-        expanded: false
-      },
-      {
-        id: 1,
-        image: "assets/imgs/syndicate/euromillions_syndicate.jpg",
-        label: "ELBA0447",
-        created: "May 29, 2018",
-        endDate: "stopped",
-        expanded: false
-      },
-      {
-        id: 2,
-        image: "assets/imgs/syndicate/eurojackpot_syndicate.jpg",
-        label: "ZULA0280",
-        created: "May 29, 2018",
-        endDate: "oneoff",
-        expanded: false
-      }
-    ]
+    this.cards = [{
+      id: 0,
+      image: "assets/image/landing-page-header@3x.png",
+      label: "Bounty syndicate",
+      expanded: false
+    }]
   }
 
   ionViewDidLoad() {
